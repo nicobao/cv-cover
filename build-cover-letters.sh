2d354ff3f120642e3d1218dcccf40cdf77f73681
Nicolas Gimenez <n.gimenez66@gmail.com>
2020-04-25 11:29:24 +0200

    Implement functionality to run only one of the cover letter

--- build-cover-letters.sh
+++ build-cover-letters.sh
@@ -1,19 +1,41 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 OUTPUT=${PWD}/output
+EXTENSION=.tex
+COVERPATH=private/cover-letters
 
+build_filename () {
+        echo "Building $filename..."
+        echo "--------------------"
+        filenameWithoutPath=$(basename $1)
+        filenameWithoutExtension="${filenameWithoutPath%.*}"
+        jobname="${filenameWithoutExtension}-Cover-Letter"
+        # Passing parameter \Filename https://tex.stackexchange.com/a/1495/213552
+        pdflatex -output-format=pdf -output-directory=$OUTPUT -jobname=$jobname "\def\filename{${1}} \input{cover-letter.tex}"
+}
 
+build_all () {
+    echo "Building all cover letters..."
+    echo "--------------------"
+    # Looping through all files in a directory with the same extension
+    # https://stackoverflow.com/a/14505622/11046178
+    for filename in $COVERPATH/*.tex; do
+        [ -f "$filename" ] || break
+        build_filename $filename
+    done
+}
 
-
-# Looping through all files in a directory with the same extension
-# https://stackoverflow.com/a/14505622/11046178
-for filename in private/cover-letters/*.tex; do
-    [ -f "$filename" ] || break
-    filenameWithoutPath=$(basename $filename)
-    filenameWithoutExtension="${filenameWithoutPath%.*}"
-    jobname="${filenameWithoutExtension}-Cover-Letter"
-    # Passing parameter \Filename https://tex.stackexchange.com/a/1495/213552
-    pdflatex -output-format=pdf -output-directory=$OUTPUT -jobname=$jobname "\def\filename{${filename}} \input{cover-letter.tex}"
-done
-
- 
+if [ -z "$1" ]
+then
+    echo "No arguments supplied"
+    build_all
+else
+    echo "Arguments supplied"
+    # Iterate over all arguments
+    # https://stackoverflow.com/a/255913/11046178
+    for filenameWithoutPathNorExtension in "$@"
+    do
+        fullFilename=$COVERPATH/$filenameWithoutPathNorExtension$EXTENSION
+        build_filename $fullFilename
+    done
+fi
