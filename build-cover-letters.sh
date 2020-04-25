feb14c32cb31f67c46b45b57310fc30103667179
Nicolas Gimenez <n.gimenez66@gmail.com>
2020-04-25 11:03:35 +0200

    Make build-cover-letters build all letters

--- build-cover-letters.sh
+++ build-cover-letters.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
+OUTPUT=${PWD}/output
 
 
 
-pdflatex -output-format=pdf -output-directory=./output -jobname=$filename "\def\filename{qonto.tex} \input{cover-letter.tex}"
+
+# Looping through all files in a directory with the same extension
+# https://stackoverflow.com/a/14505622/11046178
+for filename in private/cover-letters/*.tex; do
+    [ -f "$filename" ] || break
+    filenameWithoutPath=$(basename $filename)
+    filenameWithoutExtension="${filenameWithoutPath%.*}"
+    jobname="${filenameWithoutExtension}-Cover-Letter"
+    # Passing parameter \Filename https://tex.stackexchange.com/a/1495/213552
+    pdflatex -output-format=pdf -output-directory=$OUTPUT -jobname=$jobname "\def\filename{${filename}} \input{cover-letter.tex}"
+done
+
+ 
