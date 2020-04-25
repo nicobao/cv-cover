db52e4c04829a4f2f2fd6eef8f1dfeaf9f3b05ab
Nicolas Gimenez <n.gimenez66@gmail.com>
2020-04-25 12:04:22 +0200

    nico-cv.tex has been renamed to cv.tex - fix build-cv.sh

--- build-cv.sh
+++ build-cv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd "$(dirname "$0")"
 
-pdflatex -output-format=pdf -output-directory=./output -jobname=Nicolas-Gimenez-CV-en nico-cv.tex
+pdflatex -output-format=pdf -output-directory=./output -jobname=Nicolas-Gimenez-CV-en cv.tex
